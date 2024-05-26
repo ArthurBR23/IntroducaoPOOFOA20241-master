623c25b7f1594f19089829598c5840ebe382c81e
drawc <drawc@ART>
2024-05-26 15:27:14 -0300

    atualização

---
+++ SistemaFinanceiro/Cliente.cs
@@ -0,0 +1,53 @@
+﻿using SistemaFinanceiro.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SistemaFinanceiro
+{
+    public class Cliente
+    {
+        private string _nome, _cpf;
+        private int _anoNascimento;
+
+        public Cliente(string nome, string cpf, int anoNascimento)
+        {
+            _nome = nome;
+            _cpf = cpf;
+            _anoNascimento = anoNascimento;
+
+            if (string.IsNullOrEmpty(nome))
+            {
+                throw new ArgumentException("Nome inválido");
+            }
+        }
+
+      
+
+        public string Nome
+        {
+            get => _nome;
+            private set
+            {
+                _nome = value;
+            }
+        }
+
+        public string CPF
+        {
+            get => _cpf;
+            private set
+            {
+                _cpf = value;
+            }
+        }
+
+        public int AnoNascimento
+        {
+            get => _anoNascimento;
+        }
+    }
+
+}
---
+++ SistemaFinanceiro/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+using SistemaFinanceiro;
+using SistemaFinanceiro.Model;
+
+var cliente1 = new Cliente("", "12345678900", 2003);
+Console.WriteLine($"Cliente: {cliente1.Nome}, CPF: {cliente1.CPF}, Ano de Nascimento: {cliente1.AnoNascimento}");
+
+var conta1 = new Conta(123, 11);
+Console.WriteLine($"Conta: {conta1.Numero}, Saldo: {conta1.Saldo}");
+//conta1.Numero = 321;
+
